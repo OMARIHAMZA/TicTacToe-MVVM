bb4a6ad016e808c9601aaeee0cbaf956776f54d4
MHD HAMZA AL OMARI <36658089+OMARIHAMZA@users.noreply.github.com>
2019-08-01 13:45:52 +0300

    game models

---
+++ app/src/main/java/omari/hamza/tictactoe/models/Cell.java
@@ -0,0 +1,22 @@
+package omari.hamza.tictactoe.models;
+
+public class Cell {
+
+    private Player player;
+
+    public Cell(Player player) {
+        this.player = player;
+    }
+
+    public boolean isEmpty(){
+        return player == null || player.getValue().isEmpty() || player.getValue() == null;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+}
